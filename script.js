531e4e8c3cb9fae6f52305e2e7b0b92dc158a4ac
Kiahna Carmack <Kiahna.carmack@gmail.com>
2022-04-11 14:01:18 -0700

    final comit

--- script.js
+++ script.js
@@ -2,37 +2,47 @@ const button = document.getElementById('button');
 const inputValue = document.querySelector('.inputValue');
 const city = document.querySelector('.city');
 const temp = document.querySelector('.temp');
-const key = '&appid=9af13d161008a8ca50b9ddca7d85e250'
-
-const data = {
-    key: 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=9af13d161008a8ca50b9ddca7d85e250'
-};
+const key = '&appid=0e0021a41163b5dc5b4c2495efeda20a'
+ 
 
 if (button)
 {button.addEventListener('click', function(){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=9af13d161008a8ca50b9ddca7d85e250')
 .then(response => response.json()) 
-.then(data => console.log(data))
-
-var nameValue= data.name
-var tempValue = Math.round(data.main.temp);
-var descriptionValue=date.weather[0].description
-
-$('#city').innerHTML(city);
-$('#temp').innerHTML(temp);
-$('#description').innerHTML(description);
+.then(data => {
+    displayWeatherHeader(data)
+    console.log(data);
 })
+
+
+fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&q='+inputValue.value+'&appid=ed28904a07487f00015ab0962334985e')
+.then(response => response.json()) 
+.then(data => {
+    displayweatherCard(data)
+    console.log(data);
+})
+
+});
+};
+
+function displayWeatherHeader(weatherData) {
+document.getElementsByClassName('city')[0].textContent = weatherData.name;
+document.getElementsByClassName('temp')[0].textContent = weatherData.main.temp;
+document.getElementsByClassName('description')[0].textContent = weatherData.weather[0].description;
+document.getElementsByClassName('icons')[0].textContent = weatherData.weather[0].icon;
 }
 
+function displayweatherCard(weatherData){
+    let cardArray = document.getElementsByClassName('card');
+for (let index = 0; index < cardArray.length; index++) {
+    const element = cardArray[index];
+    element.textContent= JSON.stringify(weatherData.list[index*8])
 
+    
+}
+console.log(document.getElementsByClassName('card'))
 
-
-
-
-
-
-
-
+}
 
 
 
